03787667bca529f2bf63021d8dc4ec5b9f79fe7a
frihk <frihkian05@gmail.com>
2026-05-06 09:04:50 +0300

    array and objects

---
+++ declarations.js
@@ -0,0 +1,21 @@
+const escapeStr = "\`\\/\"'";
+const arr = [4, '2'];
+Object.freeze(arr)
+
+const obj = {
+    str : "string",
+    num : 1000,
+    bool: true,
+    undef: undefined
+};
+Object.freeze(obj)
+
+const nested = {
+    arr : [4, '2'],
+    obj : {
+        str : "nice",
+        num : 1001,
+        bool : false
+    }
+};
+Object.freeze(nested)
