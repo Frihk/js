0b8d4679d005d99f130f5a994d52e2cfa0e69c9b
frihk <frihkian05@gmail.com>
2026-05-07 09:17:00 +0300

    more or lee solution

---
+++ more-or-less.js
@@ -0,0 +1,20 @@
+function more(num) {
+    return num + 1
+}
+
+function less(num) {
+    return num - 1
+}
+
+function add(arg1, arg2) {
+    return arg1 + arg2
+}
+
+function sub(arg1, arg2) {
+    return arg1 - arg2
+}
+
+// console.log(more(5)); // Should output 6
+// console.log(less(5)); // Should output 4
+// console.log(add(5, 3)); // Should output 8
+// console.log(sub(5, 3)); // Should output 2
