140d0b365a6419e943c9b95231f1d8b24d5e9b2e
frihk <frihkian05@gmail.com>
2026-05-09 18:48:18 +0300

    remeinder

--- elementary.js
+++ elementary.js
@@ -16,22 +16,39 @@ function multiply(a, b) {
 
 function divide(a, b) {
   if (b === 0) {
-     return 0;
+    return 0;
+  }
+  if (b < 0) {
+    return -divide(a, -b);
   }
   let count = 0;
-  while (a >= b) {
-    a -= b;
-    count++;
+  if (a >= 0) {
+    while (a >= b) {
+      a -= b;
+      count++;
+    }
+  } else {
+    while (a < 0) {
+      a += b;
+      count--;
+    }
   }
   return count;
 }
 
 function modulo(a, b) {
   if (b === 0) {
-    return 0;
+    return 0; 
+    }
+  let remainder = a;
+  if (q > 0) {
+    for (let i = 0; i < q; i++) {
+      remainder -= b;
+    }
+  } else if (q < 0) {
+    for (let i = q; i < 0; i++) {
+      remainder += b;
+    }
   }
-  while (a >= b) {
-    a -= b;
-  }
-  return a;
+  return remainder;
 }
