074926baa10ae5f095c3e1b9b79cd00b8f1681a3
frihk <frihkian05@gmail.com>
2026-05-10 11:24:24 +0300

    changed from functions to regex

--- its-a-match.js
+++ its-a-match.js
@@ -1,40 +1,4 @@
-function normal(arg) {
-    for (let i = 0; i < arg.length;i++) {
-        if (i + 1 < arg.length && arg[i] + arg[i+1] === 'hi'){
-            return true
-        }
-    }
-        return false
-
-}
-
-
-function begin(arg) {
-
-    let char = arg.trim().slice(0,2)
-    if (char === 'hi'){
-        return true
-    }
-    return false
-}
-
-
-function end(arg) {
-
-    let char = arg.trim().slice(-2)
-    if (char === 'hi'){
-        return true
-    }
-    return false
-}
-
-
-
-function beginEnd(arg) {
-
-    let char = arg.trim()
-    if (char === 'hi'){
-        return true
-    }
-    return false
-}
+const normal = /hi/; 
+const begin = /^hi/; 
+const end = /hi$/; 
+const beginEnd = /^hi$/; 
