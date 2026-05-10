3eb37a32c26742cb4a822a4897f543713835001e
frihk <frihkian05@gmail.com>
2026-05-10 12:53:50 +0300

    reg checking for length match

--- same-amount.js
+++ same-amount.js
@@ -1,5 +1,8 @@
 function sameAmount(arg, reg1, reg2) {
    let check1 = arg.match(reg1) ?? []
    let check2 = arg.match(reg2) ?? []
+     if (check1.length === 0 && check2.length === 0) {
+      return false
+   }
    return check1.length === check2.length
 }
