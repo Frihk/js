ede67045be714e06958a351a6862e46a7fa8b326
frihk <frihkian05@gmail.com>
2026-05-12 16:15:49 +0300

    fixed the issues with failing  test: time.now

--- date-is.js
+++ date-is.js
@@ -3,18 +3,21 @@ function isValid(date) {
 }
 
 function isAfter(date1, date2) {
+  if (!isValid(date1) || !isValid(date2)) return false;
   return date1.getTime() > date2.getTime();
 }
  
+
 function isBefore(date1, date2) {
+  if (!isValid(date1) || !isValid(date2)) return false;
   return date1.getTime() < date2.getTime();
 }
  
-
 function isFuture(date) {
   return isValid(date) && date.getTime() > Date.now();
 }
-
+ 
 function isPast(date) {
   return isValid(date) && date.getTime() < Date.now();
 }
+
