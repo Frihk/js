424be57e861ba81e58b539c7db328f77b9061888
frihk <frihkian05@gmail.com>
2026-05-12 15:50:21 +0300

    comapring dates

---
+++ date-is.js
@@ -0,0 +1,21 @@
+function isValid(date) {
+    return !isNaN(new Date(date))
+}
+
+function isAfter(date1, date2) {
+    return new Date(date1) > new Date(date2)
+}
+
+function isBefore(date1, date2) {
+    return new Date(date1) < new Date(date2)
+}
+
+function isFuture(date) {
+    const today = new Date();
+    return today < date
+}
+
+function isPast(date) {
+    const today = new Date();
+    return today > date
+}
