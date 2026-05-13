7773b511f6d2bc736a7efc03191472309eb14a9d
frihk <frihkian05@gmail.com>
2026-05-13 19:54:47 +0300

    fixed the error

--- sunny-sunday.js
+++ sunny-sunday.js
@@ -1,4 +1,4 @@
 function sunnySunday(dd) {
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    return days[dd.getDay()];
+    return days[dd.getUTCDay()];
 }
