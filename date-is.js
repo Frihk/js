e5753c02e703249bfeafd6942d3377a07ca7c8a7
frihk <frihkian05@gmail.com>
2026-05-13 12:56:23 +0300

    fixed the isvalid error

--- date-is.js
+++ date-is.js
@@ -1,23 +1,25 @@
 function isValid(date) {
-  return (date instanceof Date || typeof date === 'number') && !isNaN(new Date(date))
+  if (typeof date === "number") return !NaN(Date)
+  return (date instanceof Date  && !isNaN(date.getTime(Date)))
 }
 
 function isAfter(date1, date2) {
   if (!isValid(date1) || !isValid(date2)) return false;
-  return date1.getTime() > date2.getTime();
+  return date1 > date2;
 }
  
 
 function isBefore(date1, date2) {
   if (!isValid(date1) || !isValid(date2)) return false;
-  return date1.getTime() < date2.getTime();
+  return date1 < date2;
 }
  
 function isFuture(date) {
-  return isValid(date) && date.getTime() > Date.now();
+  return isValid(date) && date > Date.now();
 }
  
 function isPast(date) {
-  return isValid(date) && date.getTime() < Date.now();
+  return isValid(date) && date < Date.now();
 }
+
 
