3d095c349f8f2f28264b32c1832e25a64c1d1351
frihk <frihkian05@gmail.com>
2026-05-13 15:20:47 +0300

    fixed terror in the new date

--- get-some-time.js
+++ get-some-time.js
@@ -1,5 +1,6 @@
 function firstDayWeek(num, string) {
     let day1 = new Date(string, 0, 1);
+    day1.setFullYear(parseInt(string), 0, 1);
     let dayOfWeek = day1.getDay();
     let daysToMonday = (dayOfWeek + 6) % 7;
     let targetMonday = new Date(string, 0, 1 - daysToMonday + (num - 1) * 7);
@@ -9,7 +10,7 @@ function firstDayWeek(num, string) {
 
     let day   = String(targetMonday.getDate()).padStart(2, "0");
     let month = String(targetMonday.getMonth() + 1).padStart(2, "0");
-    let yearOut = targetMonday.getFullYear();
+    let yearOut = String(targetMonday.getFullYear()).padStart(4, "0");
 
     return `${day}-${month}-${yearOut}`;
 }
