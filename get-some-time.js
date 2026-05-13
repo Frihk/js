590a47368913190eb1c00c34d2e8f1b9179e4779
frihk <frihkian05@gmail.com>
2026-05-13 15:09:25 +0300

    first day of the week

---
+++ get-some-time.js
@@ -0,0 +1,16 @@
+function firstDayWeek(num, string) {
+    let day1 = new Date(string, 0, 1);
+    let dayOfWeek = day1.getDay();
+    let daysToMonday = (dayOfWeek + 6) % 7;
+    let targetMonday = new Date(string, 0, 1 - daysToMonday + (num - 1) * 7);
+     if (targetMonday < day1) {
+        targetMonday = day1;
+    }
+
+    let day   = String(targetMonday.getDate()).padStart(2, "0");
+    let month = String(targetMonday.getMonth() + 1).padStart(2, "0");
+    let yearOut = targetMonday.getFullYear();
+
+    return `${day}-${month}-${yearOut}`;
+}
+
