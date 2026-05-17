2dc8f4a0a869def2f070b0ca553f2eb47b699dc4
frihk <frihkian05@gmail.com>
2026-05-17 20:34:57 +0300

    fix: mouse trap with cirle and box

--- mouse-trap.js
+++ mouse-trap.js
@@ -17,8 +17,8 @@ export const moveCircle = () => {
     document.addEventListener('mousemove', (event) => {
         if (trapped) return
         const circle = document.body.lastElementChild
-        circle.style.left = event.clientX + 'px'
-        circle.style.top = event.clientY + 'px'
+        circle.style.left = (event.clientX - 12.5)+ 'px'
+        circle.style.top = (event.clientY - 12.5) + 'px'
     })
 }
 
