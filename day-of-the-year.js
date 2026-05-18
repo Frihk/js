5f78e4bbc83432f26efbde9530114814388449b3
frihk <frihkian05@gmail.com>
2026-05-18 10:14:10 +0300

    fix: removed the unwanted days

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,8 +1,10 @@
-const isLeapYear = (date) => new Date(date.getFullYear(), 1, 29).getDate() === 29;
-
 const dayOfTheYear = (date) => {
     const start = Date.UTC(date.getFullYear(), 0, 0);
     const current = Date.UTC(date.getFullYear(), date.getMonth(), date.getDate());
     return (current - start) / 86400000;
 };
 
+console.log(dayOfTheYear(new Date("2024-01-01"))); // 1
+console.log(dayOfTheYear(new Date("2024-02-01"))); // 32 (31 days in January + 1)
+console.log(dayOfTheYear(new Date("2024-12-31"))); // 366 (leap year)
+console.log(dayOfTheYear(new Date("2023-12-31"))); // 365 (non-leap year)
