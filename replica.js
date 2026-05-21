235425b5b0a7aaf34de3304e127b04e79be71a93
frihk <frihkian05@gmail.com>
2026-05-22 00:12:03 +0300

    fix: repaire the replice checks

--- replica.js
+++ replica.js
@@ -1,16 +1,23 @@
-export const replica = (tar, ...sourc) => {
+const replica = (tar, ...sourc) => {
   for (let i = 0; i < sourc.length; i++) {
     const source = sourc[i];
     if (source === null || typeof source !== 'object') continue;
 
     for (let Key in source) {
-      if (source[Key] && typeof source[Key] === 'object') {
-        if (!tar[Key] || typeof tar[Key] !== 'object') {
-          tar[Key] = Array.isArray(source[Key]) ? [] : {};
+      const sourceVal = source[Key];
+      const isPlainObj = Object.prototype.toString.call(sourceVal) === '[object Object]';
+      const isArr = Array.isArray(sourceVal);
+
+      if (isPlainObj || isArr) {
+        const tarVal = tar[Key];
+        const isTarPlainObj = Object.prototype.toString.call(tarVal) === '[object Object]';
+        const isTarArr = Array.isArray(tarVal);
+        if ((isArr && !isTarArr) || (isPlainObj && !isTarPlainObj)) {
+          tar[Key] = isArr ? [] : {};
         }
-        replica(tar[Key], source[Key]);
+        replica(tar[Key], sourceVal);
       } else {
-        tar[Key] = source[Key];
+        tar[Key] = sourceVal;
       }
     }
   }
