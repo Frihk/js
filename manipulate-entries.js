25e78098264d17c4d9a1a427c7ea6cf30d443dbd
Frihk Ian <63436+fian@noreply.learn.zone01kisumu.ke>
2026-05-21 20:32:07 +0000

    Update manipulate-entries.js

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -1,46 +1,73 @@
-export const filterEntries = (obj, callback) => {
-  const oldobj = Object.entries(obj)
-  const newob =  oldobj.filter(([Key, value]) => callback([Key, value]))
-  return Object.fromEntries(newob)
+const nutritionDB = {
+  tomato:  { calories: 18,  protein: 0.9,   carbs: 3.9,   sugar: 2.6, fiber: 1.2, fat: 0.2   },
+  vinegar: { calories: 20,  protein: 0.04,  carbs: 0.6,   sugar: 0.4, fiber: 0,   fat: 0     },
+  oil:     { calories: 48,  protein: 0,     carbs: 0,     sugar: 123, fiber: 0,   fat: 151   },
+  onion:   { calories: 0,   protein: 1,     carbs: 9,     sugar: 0,   fiber: 0,   fat: 0     },
+  garlic:  { calories: 149, protein: 6.4,   carbs: 33,    sugar: 1,   fiber: 2.1, fat: 0.5   },
+  paprika: { calories: 282, protein: 14.14, carbs: 53.99, sugar: 1,   fiber: 0,   fat: 12.89 },
+  sugar:   { calories: 387, protein: 0,     carbs: 100,   sugar: 100, fiber: 0,   fat: 0     },
+  orange:  { calories: 49,  protein: 0.9,   carbs: 13,    sugar: 9,   fiber: 0.2, fat: 0.1   },
 }
 
-export const mapEntries = (obj, callback) => {
-  const oldobj = Object.entries(obj)
-  const newob =  oldobj.map(([Key, value]) => callback([Key, value]))
-  return Object.fromEntries(newob)
+function filterEntries(obj, callback) {
+    const result = {};
+    for (const [key, value] of Object.entries(obj)) {
+        if (callback([key, value])) {
+            result[key] = value;
+        }
+    }
+    return result;
 }
 
-export const reduceEntries = (obj, callback, intvalue) => {
-  const oldobj = Object.entries(obj)
-  let newob;
-  if (typeof intvalue === 'undefined'){
-    newob =  oldobj.reduce((acc, [Key, value]) => callback(acc, [Key, value]))
-  } else {
-    newob =  oldobj.reduce((acc, [Key, value]) => callback(acc, [Key, value]), intvalue)
-  }
-  return newob
+function mapEntries(obj, callback) {
+    const result = {};
+    for (const [key, value] of Object.entries(obj)) {
+        const [newKey, newValue] = callback([key, value]);
+        result[newKey] = newValue;
+    }
+    return result;
 }
 
-export const totalCalories = (arg) => {
-  const db = globalThis.nutritionDB
-  const newob = reduceEntries(arg, (acc, [Key, value]) => {
-    return acc + (db[Key].calories / 100) * value
-  }, 0)
-  return +(newob.toFixed(1))
+function reduceEntries(obj, callback, initialValue) {
+    const entries = Object.entries(obj);
+    if (entries.length === 0) return initialValue;
+    
+    let accumulator = initialValue !== undefined ? initialValue : entries[0];
+    let startIndex = initialValue !== undefined ? 0 : 1;
+    
+    for (let i = startIndex; i < entries.length; i++) {
+        accumulator = callback(accumulator, entries[i]);
+    }
+    return accumulator;
 }
 
-export const lowCarbs = (arg) => {
-  const db = globalThis.nutritionDB
-  return filterEntries(arg, ([Key, value]) => {
-    return (db[Key].carbs / 100) * value < 50
-  })
+function totalCalories(cart) {
+    const total = reduceEntries(cart, (total, [item, grams]) => {
+        const per100g = nutritionDB[item];
+        return total + (per100g.calories * grams) / 100;
+    }, 0);
+    
+    return Math.round(total * 10) / 10;
 }
 
-export const cartTotal = (arg) => {
-  const db = globalThis.nutritionDB
-  return mapEntries(arg, ([Key, value]) => {
-    return [Key, Object.fromEntries(
-      Object.keys(db[Key]).map(prop => [prop, +((db[Key][prop] / 100) * value).toFixed(3)])
-    )]
-  })
+function lowCarbs(cart) {
+    return filterEntries(cart, ([item, grams]) => {
+        const per100g = nutritionDB[item];
+        const totalCarbs = (per100g.carbs * grams) / 100;
+        return totalCarbs < 50;
+    });
+}
+
+function cartTotal(cart) {
+    return mapEntries(cart, ([item, grams]) => {
+        const per100g = nutritionDB[item];
+        const factor = grams / 100;
+        
+        const nutrients = {};
+        for (const nutrient in per100g) {
+            const value = per100g[nutrient] * factor;
+            nutrients[nutrient] = Math.round(value * 1000) / 1000;
+        }
+        return [item, nutrients];
+    });
 }
