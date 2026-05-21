e7d751b66f1530dd3c02b2824820236ec2afa0d1
frihk <frihkian05@gmail.com>
2026-05-21 22:45:37 +0300

    fix: added export key word

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -1,17 +1,17 @@
-const pick = (object, keys) => {
+export const pick = (object, keys) => {
     keys = Array.isArray(keys) ? keys : [keys]
-    const res = {}
-    for (const key of keys) {
+    export const res = {}
+    for (export const key of keys) {
         if (Object.hasOwn(object, key)) {
             res[key] = object[key]
         }
     }
     return res
 }
-const filterEntries = (object, func) => {
+export const filterEntries = (object, func) => {
     return pick(object, Object.keys(object).filter(key => func([key, object[key]])))
 }
-const mapEntries = (object, func) => {
+export const mapEntries = (object, func) => {
     const res = {}
     Object.keys(object).forEach(key => {
         const [k, v] = func([key, object[key]])
@@ -19,14 +19,14 @@ const mapEntries = (object, func) => {
     })
     return res
 }
-const totalCalories = (cart, nutritionDB) => {
+export const totalCalories = (cart, nutritionDB) => {
     return parseFloat(reduceEntries(cart, (acc, [key, value]) => acc += value / 100 * nutritionDB[key].calories, 0).toFixed(1)) 
 }
 
-const lowCarbs = (cart, nutritionDB) => {
+export const lowCarbs = (cart, nutritionDB) => {
     return filterEntries(cart, ([key, value]) => value / 100 * nutritionDB[key].carbs < 50)
 }
 
-const cartTotal = (cart, nutritionDB) => {
+export const cartTotal = (cart, nutritionDB) => {
     return mapEntries(cart, ([key, value]) => [key, mapEntries(nutritionDB[key], ([k, v]) => [k, parseFloat((value / 100 * v).toFixed(3))])])
 }
