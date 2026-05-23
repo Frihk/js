1b5590839bd56e645cdabff241b5a468a08b56d4
frihk <frihkian05@gmail.com>
2026-05-23 15:37:07 +0300

    fix: changed calculation of the x, y values

--- interpolation.js
+++ interpolation.js
@@ -1,7 +1,7 @@
 const interpolation = ({ step, start, end, duration, callback }) => {
   const interval = duration / step;
 
-  for (let i = 1; i < step; i++) {
+  for (let i = 1; i <= step; i++) {
     const x = start + (i / step) * (end - start);
     const y = (i + 1) * interval;
 
