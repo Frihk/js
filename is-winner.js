a6349cbfc82325eb2c2446350258276b5dc03f3a
frihk <frihkian05@gmail.com>
2026-05-24 09:43:50 +0300

    feat: checking for european world cup winners

---
+++ is-winner.js
@@ -0,0 +1,29 @@
+const isWinner = async (country) => {
+  let winner;
+  let results;
+
+  try {
+    winner = await db.getWinner(country);
+  } catch {
+    return 'Country never was a winner';
+  }
+
+  try {
+    results = await db.getResults(winner.id);
+  } catch {
+    return 'Country never was a winner';
+  }
+
+  if (winner.continent !== 'Europe') {
+    return 'Country is not what we are looking for because of the continent';
+  }
+
+  if (results.length < 3) {
+    return 'Country is not what we are looking for because of the number of times it was champion';
+  }
+
+  const years = results.map((r) => r.year).join(', ');
+  const scores = results.map((r) => r.score).join(', ');
+
+  return `${country} won the FIFA World Cup in ${years} winning by ${scores}`;
+};
