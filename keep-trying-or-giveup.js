58441699f590140bee2815cfd24d89d82ff94c0e
frihk <frihkian05@gmail.com>
2026-05-24 08:11:26 +0300

    feat: implemented a func rety and timeout

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,3 +1,22 @@
 const retry = (count, callback) => {
-     
-}
+  return async (...args) => {
+    for (let i = 0; i <= count; i++) {
+      try {
+        return await callback(...args);
+      } catch (err) {
+        if (i === count) throw new Error('All retries failed');
+      }
+    }
+  };
+};
+
+const timeout = (delay, callback) => {
+  return async (...args) => {
+    const timer = new Promise((_, reject) =>
+      setTimeout(() => reject(new Error('timeout')), delay)
+    );
+
+    return Promise.race([callback(...args), timer]);
+  };
+};
+
